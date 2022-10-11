6eccc982cfa3690f453bb36d7cb4edaa096f50fb
neo <hayounn.etd@gmail.com>
2022-10-11 17:56:31 +0100

    Create an show view for the recipes acoording to the wireframe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,8 @@ class RecipesController < ApplicationController
     def index
         @recipes = Recipe.all
     end
+    def show
+        @recipe = Recipe.find(params[:id])
+        @foods = Food.all
+    end
 end
