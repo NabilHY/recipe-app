f856668a55c96b0428bccfd5c836fc8fbd6361d9
Sanja <sanja1mandic@gmail.com>
2022-10-11 14:14:43 +0200

    Create controller and view file for food

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'tzinfo-data'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
 
+gem 'sass-rails'
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'foods/index'
+  get 'foods/show'
+  resources :recipes, only: [:create, :destroy, :index, :show]
+  resources :foods, only: [:create, :destroy, :index, :show]
 end
