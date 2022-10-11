77549e22fd6128674b0a7437a57a496b52726c9b
Sanja <sanja1mandic@gmail.com>
2022-10-11 13:29:19 +0200

    fix db bug

--- db/migrate/20221011102022_change_time_to_be_string_in_recipes.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTimeToBeStringInRecipes < ActiveRecord::Migration[7.0]
-  def change
-    change_column :recipes, :cooking_time, :string
-    change_column :recipes, :preparation_time, :string 
-  end
-end
