44cfead4272fe09fc6ec05319089b18d8a531fd4
neo <hayounn.etd@gmail.com>
2022-10-11 18:11:55 +0100

    Generate migration to add price column to foods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_102022) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_170444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_102022) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
