e6734cfa6df0e55aa8749237ab648b0930af9d8b
neo <hayounn.etd@gmail.com>
2022-10-12 07:13:01 +0100

    Edit recipe controler to include only foods that belong to a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,27 @@
 class RecipesController < ApplicationController
     def index
-        @recipes = Recipe.all
+        @user = User.find(params[:user_id])
+        @recipes = @user.recipes
     end
     def show
+        @user = User.find(params[:user_id])
         @recipe = Recipe.find(params[:id])
-        @foods = Food.all
+        @foods = Recipe_food.where(recipe_id: @recipe.id) 
+    end
+    def new
+        @recipe = Recipe.new
+    end
+    def create
+        @recipe = current_user.recipes.new(recipe_params)
+        respond_to do |format|
+            if @recipe.save
+                format.html { redirect_to user_recipes_path, notice: 'Recipe was successfully created.' }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+            end
+        end
+    end
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :public)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index, :show]
+  resources :users do
+    resources :recipes, only: [:index, :show, :new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
