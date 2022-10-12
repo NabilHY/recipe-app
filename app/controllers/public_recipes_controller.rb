fc5621123d1ab12a149c9f9260c7aec300323dda
neo <hayounn.etd@gmail.com>
2022-10-12 18:14:17 +0100

    Make an index page for public recipes with functionnality

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,5 +2,9 @@ class PublicRecipesController < ApplicationController
     def index
         @recipes = Recipe.all
         @public_recipes = @recipes.select { |recipe| recipe.public == true }
+        @prices = []
+        @public_recipes.each do |recipe|
+            @prices << recipe.recipe_foods.map { |recipe_food| recipe_food.food.price }.sum
+        end
     end
 end
