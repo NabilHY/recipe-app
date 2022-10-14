bb4f319bc48cf63febce654f6c4f4dc4a35dfb16
neo <hayounn.etd@gmail.com>
2022-10-14 16:53:02 +0100

    Fix linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,5 +6,4 @@ class Food < ApplicationRecord
   validates :measurement_unit, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
-
 end
